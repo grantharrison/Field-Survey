490829c3edc37f6de65d9afe642ea298839f2df1
grantharrison <gthgg6@mail.missouri.edu>
2019-11-21 17:18:17 -0600

    Initial Commit

---
+++ Field Survey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Field Survey
+//
+//  Created by Grant Harrison on 11/21/19.
+//  Copyright © 2019 Grant Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
